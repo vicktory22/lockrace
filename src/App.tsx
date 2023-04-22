a5b3c69790a0d5df3f73154512ef98b2090ccf8e
Tim Vick <t.vick@yahoo.com>
2023-04-22 15:12:01 -0700

    feat: setup auth layout (#7)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes, Router } from '@solidjs/router';
 import type { Component } from 'solid-js';
+import { AuthLayout } from './pages/AuthLayout';
 import { Dashboard } from './pages/Dashboard';
 import { Games } from './pages/Games';
 import { Me } from './pages/Me';
@@ -8,9 +9,11 @@ const App: Component = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" component={Dashboard} />
-        <Route path="/games" component={Games} />
-        <Route path="/me" component={Me} />
+        <Route path="/" component={AuthLayout}>
+          <Route path="/" component={Dashboard} />
+          <Route path="/games" component={Games} />
+          <Route path="/me" component={Me} />
+        </Route>
       </Routes>
     </Router>
   );
