8abc113f330d07fc5844b6c43bdff6070bbff52f
Tim Vick <t.vick@yahoo.com>
2023-04-22 12:58:27 -0700

    feat: add testing suite (#2)

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@solidjs/testing-library';
+import App from '../App';
+import { describe, expect, it } from 'vitest';
+import '@testing-library/jest-dom';
+
+describe('App', () => {
+  it('should render the app', () => {
+    const { getByText } = render(() => <App />);
+    expect(getByText('Hello, world!')).toBeInTheDocument();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
@@ -8,5 +11,9 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+  },
+  test: {
+    environment: 'jsdom',
+    transformMode: { web: [/\.[jt]sx?$/] },
   },
 });
