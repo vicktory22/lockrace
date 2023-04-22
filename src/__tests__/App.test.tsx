77da738b8e33f8ed764877869728956e7be7e524
Tim Vick <t.vick@yahoo.com>
2023-04-22 15:54:33 -0700

    feat: add lazy loaded routes

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,8 +4,8 @@ import { describe, expect, it } from 'vitest';
 import '@testing-library/jest-dom';
 
 describe('App', () => {
-  it('should render the app', () => {
-    const { getByText } = render(() => <App />);
-    expect(getByText('Dashboard')).toBeInTheDocument();
+  it('should lazy load all routes', () => {
+    const { queryByText } = render(() => <App />);
+    expect(queryByText('Dashboard')).toBeNull();
   });
 });
