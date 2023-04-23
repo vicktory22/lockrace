2a7d641b13977c569687cde263ac0f25c4d5a0ec
Tim Vick <t.vick@yahoo.com>
2023-04-23 09:14:02 -0700

    feat: add auth check

--- src/pages/AuthLayout.tsx
+++ src/pages/AuthLayout.tsx
@@ -1,7 +1,17 @@
-import { Outlet } from "@solidjs/router";
+import { Outlet, useNavigate } from "@solidjs/router";
+import { createEffect } from "solid-js";
 import { BottomNav } from "../components/BottomNav";
+import { isAuthenticated } from "../services/auth/authenticator";
 
 export function AuthLayout() {
+  const navigate = useNavigate();
+
+  createEffect(() => {
+    if (!isAuthenticated()) {
+      navigate('/login', { replace: true });
+    }
+  });
+
   return (
     <>
       <div class="h-full pb-12">
---
+++ src/services/auth/authenticator.ts
@@ -0,0 +1,7 @@
+import Cookie from 'js-cookie';
+
+const AUTH_COOKIE = '_did';
+
+export function isAuthenticated() {
+  return !!Cookie.get(AUTH_COOKIE);
+}
