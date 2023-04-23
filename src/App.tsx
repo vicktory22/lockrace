36a2c1db02eca1576bab4e903205dcbf80b97582
Tim Vick <t.vick@yahoo.com>
2023-04-23 08:36:40 -0700

    feat: add the base login page (#9)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes, Router } from '@solidjs/router';
 import { Component, lazy } from 'solid-js';
 import { AuthLayout } from './pages/AuthLayout';
+const Login = lazy(() => import('./pages/Login'));
 const Dashboard = lazy(() => import('./pages/Dashboard'));
 const Games = lazy(() => import('./pages/Games'));
 const Me = lazy(() => import('./pages/Me'));
@@ -9,6 +10,7 @@ const App: Component = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/login" component={Login} />
         <Route path="/" component={AuthLayout}>
           <Route path="/" component={Dashboard} />
           <Route path="/games" component={Games} />
