9175120370a848077eea70af342e9e98ae4ffaac
Tim Vick <t.vick@yahoo.com>
2023-04-26 08:33:04 -0700

    feat: add dummy auth client for tests

--- src/services/auth/authenticator.ts
+++ src/services/auth/authenticator.ts
@@ -4,16 +4,30 @@ import { ResultAsync } from "neverthrow";
 
 const AUTH_COOKIE = "_did";
 
-const client = new Magic(import.meta.env.VITE_AUTH_PUBLIC_KEY);
+const client = getClient();
 
 export function isAuthenticated() {
   return !!Cookie.get(AUTH_COOKIE);
 }
 
-export function loginUser(phoneNumber: string): ResultAsync<string | null, unknown> {
+export function loginUser(phoneNumber: string): ResultAsync<string | void | null, unknown> {
   return ResultAsync.fromPromise(client.auth.loginWithSMS({ phoneNumber }).then(writeLoginCookie), (error) => error);
 }
 
 function writeLoginCookie(did: string | null) {
   Cookie.set(AUTH_COOKIE, did || ""); // TODO revist this how to handle null
+}
+
+function getClient() {
+  if (import.meta.env.MODE === "test") {
+    return {
+      auth: {
+        loginWithSMS: async ({ phoneNumber: _ }: { phoneNumber: string }) => {
+          return Promise.resolve("did:ethr:0x1234567890123456789012345678901234567890");
+        },
+      },
+    };
+  }
+
+  return new Magic(import.meta.env.VITE_AUTH_PUBLIC_KEY);
 }
