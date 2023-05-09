b10ed94118339101dc4c707b2143e1b6edcb98a6
Tim Vick <t.vick@yahoo.com>
2023-05-09 15:16:32 -0700

    chore: update compatibility-date flag (#34)

--- workers/cron/refresh-games.ts
+++ workers/cron/refresh-games.ts
@@ -18,6 +18,7 @@ const handler: ExportedHandler<Env> = {
       return HttpResponses.internalServerError('Unable to fetch games.');
     }
 
+
     return HttpResponses.ok(JSON.stringify(gamesResponse.unwrap()));
   },
 };
