d14a01f11cb5d8d9429538f39c326406ae98de7f
Tim Vick <t.vick@yahoo.com>
2023-06-04 10:20:09 -0700

    feat: add games page

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,9 +1,21 @@
-import { JSX } from "solid-js";
+import { fetchTeams } from "../services/games/teams";
+import { JSX, Show, createResource } from "solid-js";
 
 export default function Games(): JSX.Element {
+  const [teams] = createResource(fetchTeams);
+
   return (
-    <article class="prose">
+    <article class="prose w-full">
       <h1>Games</h1>
+      <Show when={teams.loading}>Loading...</Show>
+      <Show when={teams.error}>Error: {teams.error.message}</Show>
+      <Show when={teams()}>
+        <ul>
+          {teams()?.rows.map((team) => (
+            <li>{team.name}</li>
+          ))}
+        </ul>
+      </Show>
     </article>
   );
 }
